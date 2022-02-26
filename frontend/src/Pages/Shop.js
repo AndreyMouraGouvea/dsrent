8b98bd842278a96c53bfd4a7e90ba7c240058929
AndreyMouraGouvea <andreygouveia.05@gmail.com>
2022-02-25 21:19:35 -0300

    login page

--- frontend/src/Pages/Shop.js
+++ frontend/src/Pages/Shop.js
@@ -9,6 +9,7 @@ import Find from './Find';
 import New from '../components/New';
 import House from '../components/House';
 import Recommended from '../components/Recommended';
+import Login from '../components/Login';
 
 
 
@@ -28,127 +29,13 @@ function Shop() {
             showsVerticalScrollIndicator={false}
             style={{ backgroundColor: '#FFF' }}
         >
-
-            <View style={styles.header}>
-                <View style={styles.inputArea}>
-                    <Feather
-                        name="search"
-                        size={24}
-                        color='#000'
-                    />
-                    <TextInput
-                        placeholder='O que está procurando?'
-                        style={styles.input}
-                    />
-                </View>
-
-            </View>
-
-            {/* flat list => api */}
-
-            <View style={styles.contentNew}>
-                <Text style={styles.title}>Novidades</Text>
-
-
-            </View>
-
             <ScrollView
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                style={{ paddingHorizontal: 15 }}
+            showsHorizontalScrollIndicator={false}
+            style={{ paddingHorizontal: 15 }}
             >
-
-                <New
-                    cover={require('../assets/house1.jpg')}
-                    name='Casa de Praia'
-                    description='Casa nova uma quadra do mar, lugar seguro e monitorado 24 horas'
-                    price='R$ 824,90'
-                    onPress={() => navigation.navigate('detail')}
-
-                />
-
-                <New
-                    cover={require('../assets/house2.jpg')}
-                    name='Casa Floripa'
-                    description='Casa nova uma quadra do mar, lugar seguro e monitorado 24 horas'
-                    price='R$ 1500,00'
-                    onPress={() => navigation.navigate('detail')}
-
-
-                />
-
-                <New
-                    cover={require('../assets/house3.jpg')}
-                    name='Rancho SP'
-                    description='Casa nova uma quadra do mar, lugar seguro e monitorado 24 horas'
-                    price='R$ 953,90'
-                    onPress={() => navigation.navigate('detail')}
-
-
-                />
-
-
+                <Login />
 
             </ScrollView>
-
-
-            <View style={{ flexDirection: 'row', marginBottom: 10, alignItems: 'center' }}>
-                <Text style={[styles.title, { marginTop: 20 }]}>Próximo de você</Text>
-            </View>
-
-            <ScrollView
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                style={{ paddingHorizontal: 15 }}
-            >
-                <House
-                    cover={require('../assets/house4.jpg')}
-                    price='R$ 954,80'
-                    description='Sobrado, três dormitórios, dois banheiros, piscina e churrasqueira'
-                />
-                <House
-                    cover={require('../assets/house5.jpg')}
-                    price='R$ 1030,90'
-                    description='Casa de frente ao mar, três suítes e cinco vagas de garagem'
-                />
-                <House
-                    cover={require('../assets/house6.jpg')}
-                    price='R$ 1200,90'
-                    description='Possui 45m², uma suíte,
-             cozinha equipada e uma linda vista da vida noturna'
-                />
-
-
-            </ScrollView>
-
-            <Text style={[styles.title, { marginTop: 20 }]}>
-                Dica do Dia
-            </Text>
-
-            <ScrollView
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                style={{ paddingHorizontal: 15 }}
-            >
-
-                <Recommended
-                    cover={require('../assets/house1.jpg')}
-                    house='Casa Floripa'
-                    offer='20%'
-                />
-                <Recommended
-                    cover={require('../assets/house4.jpg')}
-                    house='Casa São Paulo'
-                    offer='15%'
-                />
-                <Recommended
-                    cover={require('../assets/house6.jpg')}
-                    house='Rancho na Praia'
-                    offer='10%'
-                />
-
-            </ScrollView>
-
     
         </ScrollView>
 
