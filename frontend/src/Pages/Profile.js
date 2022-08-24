0ea13ef3ef34e0804aed3a6f4eec59cd440e6822
AndreyMouraGouvea <andreygouveia.05@gmail.com>
2022-08-24 15:07:11 -0300

    profile page init

--- frontend/src/Pages/Profile.js
+++ frontend/src/Pages/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native'
 import { useNavigation } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons'
 
 
 function Profile() {
@@ -9,23 +10,64 @@ function Profile() {
 
     return (
         <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.headerText}>Tela de Perfil</Text>
+            {/* <View style={styles.header}>
+                <Text style={styles.headerText}>Teste</Text>
+            </View> */}
+            <View style={styles.profileContainer}>
+                <Feather
+                    name='user'
+                    size={26}
+                    color='#BB86FC'
+                    style={styles.icon}
+                />
             </View>
+            <View style={styles.buttonEditContainer}>
+                <TouchableOpacity style={styles.buttonSend}
+                    onPress={() => alert('Enviar Foto')}
+                // () => navigation.navigate('home')
+                >
+                    <Text style={styles.buttonText}>Enviar Foto</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.buttonEdit}
+                    onPress={() => alert('Editar perfil')}
+                // () => navigation.navigate('home')
+                >
+                    <Text style={styles.buttonText}>Editar Perfil</Text>
+                </TouchableOpacity>
+            </View>
+
             <View style={styles.inputContainerLogin}>
                 <TextInput style={styles.input}
-                    placeholder='Digite seu Login'
+                    placeholder='Nome que a pessoa escolheu'
                     placeholderTextColor={'#FFF'}
+                    editable={false}
                 />
             </View>
             <View style={styles.inputContainerPassword}>
                 <TextInput style={styles.input}
-                    placeholder='Digite sua Senha'
+                    placeholder='E-mail que a pessoa escolheu'
                     placeholderTextColor={'#FFF'}
+                    editable={false}
+                />
+            </View>
+            <View style={styles.inputContainerLogin}>
+                <TextInput style={styles.input}
+                    placeholder='Telefone que a pessoa escolheu'
+                    placeholderTextColor={'#FFF'}
+                    editable={false}
+                />
+            </View>
+            <View style={styles.inputContainerPassword}>
+                <TextInput style={styles.input}
+                    placeholder='Endereço que a pessoa escolheu'
+                    placeholderTextColor={'#FFF'}
+                    editable={false}
                 />
             </View>
 
-            <View style={styles.buttonContainer}>
+            {/* <View style={styles.buttonContainer}>
                 <TouchableOpacity style={styles.button}
                 onPress={ () => navigation.navigate('home')}
                 >
@@ -35,7 +77,7 @@ function Profile() {
                 onPress={ () => navigation.navigate('register')}>
                     <Text style={styles.buttonText}>Cadastrar</Text>
                 </TouchableOpacity>
-            </View>
+            </View> */}
         </View>
 
 
@@ -51,6 +93,15 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#121212'
 
+    },
+    profileContainer: {
+        width: '20%',
+        height: '12%',
+        borderRadius: 50,
+        borderWidth: 3,
+        borderColor: '#BB86FC',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     inputContainerLogin: {
         width: '90%',
@@ -104,13 +155,18 @@ const styles = StyleSheet.create({
 
 
     },
+    buttonEditContainer: {
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+    },
     buttonContainer: {
         width: '100%',
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-around',
-        marginVertical: '20%'
-
+        marginVertical: '10%'
     },
     button: {
         width: '40%',
@@ -122,6 +178,26 @@ const styles = StyleSheet.create({
         height: 70,
         paddingHorizontal: 10,
 
+    },
+    buttonSend: {
+        width: '50%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#BB86FC',
+        borderRadius: 10,
+        elevation: 2,
+        height: 70,
+        paddingHorizontal: 5,
+    },
+    buttonEdit: {
+        width: '50%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#3706b3',
+        borderRadius: 10,
+        elevation: 2,
+        height: 70,
+        paddingHorizontal: 5,
     },
     buttonText: {
         color: '#FFF',
