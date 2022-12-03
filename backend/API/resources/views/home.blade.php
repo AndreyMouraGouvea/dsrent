22c06c57c16c0ea8ff998f8cfecf2a19e2d0a2e7
Pedrorcpla <pedrorcpla@gmail.com.br>
2022-12-02 21:31:50 -0300

    Adicionado botão de acesso ao mapa

--- backend/API/resources/views/home.blade.php
+++ backend/API/resources/views/home.blade.php
@@ -4,6 +4,7 @@
     <div class="info">
         <h3>O melhor do país<br>na palma da sua mão!</h3>
         <p>Um APP para te deixar por dentro de todos os eventos próximos de você!</p>
+        <a href="./mapa" class="git"><img align="center" height="30" width="30" src="{{ asset('img/marker.png') }}"> Veja o melhor próximo de você</a>
         <a href="https://github.com/AndreyMouraGouvea/dsrent" target="_blank" class="git"><img align="center" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg"> Acessar GitHub</a>
     </div>
     <div class="smartphone">
